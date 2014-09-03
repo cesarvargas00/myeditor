e7f45b5660206d6edd16d4db0908b1c7173cb2cd
Cesar Vargas <cesarvargas@Cesars-MacBook-Pro.local>
2014-09-03 13:13:57 -0400

    commiting stuff

--- server/api/challenge/index.js
+++ server/api/challenge/index.js
@@ -9,7 +9,7 @@ router.get('/',auth.isAuthenticated(), controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
+router.patch('/:id', controller.merge);
 router.delete('/:id', controller.destroy);
 
 module.exports = router;
