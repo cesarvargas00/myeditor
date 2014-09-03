ba0b7b45d179b7d9a09d57043081b3d76bcbaa02
Cesar Vargas <cesarvargas@Cesars-MacBook-Pro.local>
2014-09-02 21:31:05 -0400

    use mongohq in dev if available

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,8 +5,10 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/myeditor-dev'
-  },
-
+    uri:    process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+            'mongodb://localhost/myeditor-dev'
+  }
   seedDB: true
 };
