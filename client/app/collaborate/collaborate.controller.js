e3f01f072b329f0c5b3df1f6c7ba7da027119109
Cesar Vargas <cesarvargas@Cesars-MacBook-Pro.local>
2014-09-03 14:42:30 -0400

    collaborating run

--- client/app/collaborate/collaborate.controller.js
+++ client/app/collaborate/collaborate.controller.js
@@ -1,11 +1,18 @@
 'use strict';
 
 angular.module('myEditorApp')
-  .controller('CollaborateCtrl', function ($scope,$http,$routeParams,Auth) {
-     $scope.modes = ['javascript', 'java', 'c_cpp', 'python'];
+    .controller('CollaborateCtrl', function($scope, $http, $routeParams, Auth, $activityIndicator) {
+        $scope.modes = ['java', 'c_cpp'];
         $scope.code = {
-            content: '',
-            currentMode: 'javascript'
+          currentMode:'java',
+          run:{
+            'java':'',
+            'c_cpp':''
+          },
+          solution:{
+            'java':'',
+            'c_cpp':''
+          }
         };
         $scope.tests = {
             content: '',
@@ -23,29 +30,49 @@ angular.module('myEditorApp')
         $scope.codeOptions = {
             mode: $scope.code.currentMode,
             onLoad: function(_ace) {
-              if(!ranOnce){
-                ranOnce = true;
-              } else{
-                return 0;
-              }
-              var fireRef = new Firebase('my-editor.firebaseio.com/collaborate/'+$routeParams.sId);
-              Auth.isLoggedInAsync(function(){
-                    var firepad = Firepad.fromACE(fireRef, _ace,{userId:Auth.getCurrentUser().name});
-                  console.log('outside');
-                  fireRef.on('value',function(dataSnapshot){
-                  console.log('inside',dataSnapshot);
-                   var temp = [];
-                   dataSnapshot.child('users').forEach(function(d){
-                     temp.push(d.name());
-                   });
-                   $scope.userList = temp;
-                   _.defer(function(){$scope.$apply()});
-                });
-                $scope.codeModeChanged = function() {
-                    _ace.getSession().setMode("ace/mode/" + $scope.code.currentMode.toLowerCase());
-                };
-              })
+                if (!ranOnce) {
+                    ranOnce = true;
+                } else {
+                    return 0;
+                }
+                var fireRef = new Firebase('my-editor.firebaseio.com/collaborate/' + $routeParams.sId);
+                Auth.isLoggedInAsync(function() {
+                    var firepad = Firepad.fromACE(fireRef, _ace, {
+                        userId: Auth.getCurrentUser().name
+                    });
+                    console.log('outside');
+                    fireRef.on('value', function(dataSnapshot) {
+                        console.log('inside', dataSnapshot);
+                        var temp = [];
+                        dataSnapshot.child('users').forEach(function(d) {
+                            temp.push(d.name());
+                        });
+                        $scope.userList = temp;
+                        _.defer(function() {
+                            $scope.$apply()
+                        });
+                    });
+                    $scope.codeModeChanged = function() {
+                        _ace.getSession().setMode("ace/mode/" + $scope.code.currentMode.toLowerCase());
+                    };
+                })
 
             }
         };
-  });
+
+        $scope.run = function() {
+            $scope.submitted = true;
+            $scope.problem.run = $scope.code.run;
+            $scope.problem.solution = $scope.code.solution;
+            $activityIndicator.startAnimating();
+            $http({
+                method: 'POST',
+                url: 'http://54.88.184.168/run',
+                data: $scope.problem
+            }).success(function(data) {
+                $scope.submitted = false;
+                $activityIndicator.stopAnimating();
+                $scope.output = data;
+            });
+        };
+    });
